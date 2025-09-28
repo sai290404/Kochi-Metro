e566fe5be6daa70ae6c8ddaba6d6d60d546de242
sai290404 <g.sai2904@gmail.com>
2025-09-28 18:49:20 +0530

    Update api.js

--- kochi-metro-induction-system-main/frontend/src/services/api.js
+++ kochi-metro-induction-system-main/frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://kochi-metro-rail.onrender.com';
+const API_BASE_URL = 'https://kochi-metro-system.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
